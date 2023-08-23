0b3aa2f192d2bdb55e03c960a117bdb0ffeeb8ee
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-23 18:47:27 -0300

    Add files via upload

---
+++ EX9POO/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX9POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.Write("Digite a base do retângulo: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+
+            retangulo.ExibirMensagemArea();
+        }
+    }
+}
---
+++ EX9POO/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX9POO
+{
+    internal class Retangulo
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+        public void ExibirMensagemArea()
+        {
+            double area = CalcularArea();
+            Console.WriteLine("Área do retângulo: " + area);
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+    }
+}
